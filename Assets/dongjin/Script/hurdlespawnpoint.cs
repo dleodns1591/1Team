a81545d90b5f5ba568e9713e09d1d6607ccbe67b
kgoon <sdh210203@sdh.hs.kr>
2022-01-03 15:43:29 +0900

    살림1

--- Assets/dongjin/Script/hurdlespawnpoint.cs
+++ Assets/dongjin/Script/hurdlespawnpoint.cs
@@ -6,22 +6,37 @@ public class hurdlespawnpoint : MonoBehaviour
 {
     public float spawntime;
     public GameObject hurdle;
+    public GameObject hurdle2;
     private float currenttime;
     private float spawnpoint;
+    private Vector3 positions;
     private void Start()
     {
         currenttime = spawntime;
     }
     private void Update()
     {
-        if(currenttime <= 0)
+        if (currenttime <= 0)
         {
             spawnpoint = Random.Range(3.5f, 9.5f);
             currenttime = spawntime;
             transform.position = new Vector3(13, spawnpoint, 0);
-            Instantiate(hurdle, transform.position, Quaternion.Euler(0,0,180));
-            transform.position = new Vector3(13, spawnpoint-14, 0);
+            if (spawnpoint < 6.5f)
+            {
+                float dd = Random.Range(1.5f, 3.5f);
+                positions = new Vector3(transform.position.x, transform.position.y - dd, 0);
+
+            }
+            else
+            {
+                float dd = Random.Range(9.5f, 12f);
+                positions = new Vector3(transform.position.x, transform.position.y - dd, 0);
+            }
+            Instantiate(hurdle, transform.position, Quaternion.Euler(0, 0, 180));
+            Instantiate(hurdle2, positions, Quaternion.Euler(0, 0, 180));
+            transform.position = new Vector3(13, spawnpoint - 14, 0);
             Instantiate(hurdle, transform.position, hurdle.transform.rotation);
+            Instantiate(hurdle2, positions, Quaternion.Euler(0, 0, 180));
         }
         else
         {
